4013532e727bb656df2fece7359c97c038c07f49
safdar-azeem <safdarazeem00@gmail.com>
2026-01-05 23:23:15 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,14 @@ import connectDB from './config/db'
 
 const PORT = process.env.PORT || 4003
 
-connectDB()
+const startServer = async () => {
+  await connectDB()
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  })
+}
+
+startServer()
 
 export default app
