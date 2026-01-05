4ac425a0d44e38136ae164f253312edeea7ac3b3
safdar-azeem <safdarazeem00@gmail.com>
2026-01-05 22:52:19 +0500

    Add server.ts

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import dotenv from 'dotenv'
+dotenv.config({ path: path.join(__dirname, '..', '.env') })
+
+import app from './app'
+import connectDB from './config/db'
+
+const PORT = process.env.PORT || 4003
+
+if (require.main === module) {
+  connectDB()
+
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  })
+}
+
+export default app
